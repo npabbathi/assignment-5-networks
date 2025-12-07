dd1db840809835ee36c925b29bedbee435f35107
Karnika <karnika.choudhury@gmail.com>
2025-12-07 12:37:41 -0600

    preliminary code diffie hellman written having error peer connnection reset

--- diffie_hellman_client.py
+++ diffie_hellman_client.py
@@ -5,10 +5,22 @@ import random
 from pathlib import Path
 from typing import Tuple
 
-
 # TODO feel free to use this helper or not
 def send_common_info(sock: socket.socket, server_address: str, server_port: int) -> Tuple[int, int]:
     # TODO: Connect to the server and propose a base number and prime
+    try:
+        # Create a socket object
+            # Connect to the server
+            sock.connect((server_address, server_port))
+            message = "5 23"
+            # Send the message
+            sock.sendall(message.encode('utf-8'))
+            # Receive the response
+            response = sock.recv(10000)
+            
+            return [5, 23]
+    except Exception as e:
+        return f"An error occurred: {e}"
     # TODO: You can generate these randomly, or just use a fixed set
     # TODO: Return the tuple (base, prime modulus)
     pass
@@ -16,20 +28,25 @@ def send_common_info(sock: socket.socket, server_address: str, server_port: int)
 # Do NOT modify this function signature, it will be used by the autograder
 def dh_exchange_client(server_address: str, server_port: int) -> Tuple[int, int, int, int]:
     # TODO: Create a socket 
-    
     # TODO: Send the proposed base and modulus number to the server using send_common_info
+    base, mod = send_common_info(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_address, server_port)
 
     # TODO: Come up with a random secret key
+    client_secret = random.randint(1, 100)
 
     # TODO: Calculate the message the client sends using the secret integer.
-
-    # TODO: Exhange messages with the server
-
+    client_message = base ** client_secret % mod
+    # TODO: Exchange messages with the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_address, server_port))
+        s.sendall(str(client_message).encode('utf-8'))
+        data = s.recv(1024)
     # TODO: Calculate the secret using your own secret key and server message
-    
+    shared_secret = data.decode('utf-8') ** client_secret % mod
     # TODO: Return the base number, the modulus, the private key, and the shared secret
+    print("client return", base, mod, client_secret, shared_secret)
+    return (base, mod, client_secret, shared_secret)
 
-    pass
 
 
 def main(args):
--- diffie_hellman_server.py
+++ diffie_hellman_server.py
@@ -5,27 +5,56 @@ import random
 from pathlib import Path
 from typing import Tuple
 
-
 # TODO feel free to use this helper or not
-def receive_common_info() -> Tuple[int, int]:
+def receive_common_info(server_address: str, server_port: int) -> Tuple[int, int]:
     # TODO: Wait for a client message that sends a base number.
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((server_address, server_port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                data = data.decode('utf-8')
+                a, b = map(int, data.split())
+    s.close()
+    return (a, b)
     # TODO: Return the tuple (base, prime modulus)
-    pass
+        
 
 # Do NOT modify this function signature, it will be used by the autograder
 def dh_exchange_server(server_address: str, server_port: int) -> Tuple[int, int, int, int]:
     # TODO: Create a server socket. can be UDP or TCP.
-
     # TODO: Read client's proposal for base and modulus using receive_common_info
+    base, modulus = receive_common_info(server_address, server_port)
 
     # TODO: Generate your own secret key
-
+    server_secret = random.randint(1, 100)
     # TODO: Exchange messages with the client
+    server_message = base ** server_secret % modulus
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((server_address, server_port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(str(server_message).encode('utf-8'))
 
     # TODO: Compute the shared secret.
+    shared_secret = data.decode('utf-8') ** server_secret % modulus
 
     # TODO: Return the base number, prime modulus, the secret integer, and the shared secret
-    pass
+    print("server return", base, modulus, server_secret, shared_secret)
+    return (base, modulus, server_secret, shared_secret)
 
 def main(args):
     dh_exchange_server(args.address, args.port)
